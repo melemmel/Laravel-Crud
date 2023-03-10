eb8a24fc12f718cf62ede56c2f90f7eb0e5016bd
melemmel <ramosrommel2001@gmail.com>
2023-03-10 21:07:12 +0800

    Make some changes

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 CRUD</title>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}" />
+</head>
+
+<body>
+
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+
+</html>
